d03c77bc9edfd9ee771f206a1fb0939d4404141f
Frédéric Collonval <fcollonval@gmail.com>
2021-02-24 16:17:26 +0100

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -15,7 +15,7 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-
+# Small changes
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
